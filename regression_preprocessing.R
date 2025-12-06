23d6ac60eb5fad78ee533ba53b28b4518d34af44
Angela Luo <angela1uo@g.ucla.edu>
2025-12-06 13:02:08 -0800

    Added summarization for categories

--- regression_preprocessing.R
+++ regression_preprocessing.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 library(rpart)
+library(mlr3)
+library(mlr3learners)
+library(mlr3verse)
+library(mlr3pipelines)
+library(ggplot2)
+library(xgboost)
+library(dplyr)
+library(rpart)
+library(broom)
+library(glmnet)
+library(stringr)
 library(janitor)
 
 # Load datasets
@@ -8,6 +19,8 @@ survey <- read_csv("data/regression/survey_train_test.csv")
 
 purchases <- purchases |> clean_names(sep_out=".")
 survey <- survey |> clean_names(sep_out=".")
+
+purchases$category[purchases$category == ""] <- NA
 
 ## Umbrella groups for the category variable
 purchases <- purchases %>%
@@ -79,11 +92,32 @@ purchases <- purchases %>%
     )
   )
 
-# flatten purchases to one row per ID???
+# the top 5 categories for each ID 
+top_categories_by_id <- purchases %>%
+  group_by(survey.response.id, category) %>%
+  summarise(n = n(), .groups = 'drop_last') %>%
+  arrange(desc(n)) %>%
+  slice_head(n = 5) %>%
+  summarise(top.categories = paste(category, collapse = ", ")) %>%
+  ungroup()
 
+# the shipping address state for each ID 
+state_by_id <- purchases %>%
+  group_by(survey.response.id, shipping.address.state) %>%
+  summarise(n = n(), .groups = 'drop_last') %>%
+  arrange(desc(n)) %>%
+  slice_head(n = 1) %>% 
+  select(survey.response.id, shipping.address.state) %>%
+  ungroup()
+
+# drop irrelevant categories 
 purchases <- purchases |>
-  select(!c(title, asin.isbn.product.code)) 
+  select(!c(title, asin.isbn.product.code, category)) 
 
+# flatten purchases to one row per ID
+
+# summarize the total spent by the user 
+# and the average amount spent per order for the user
 purchases <- purchases |> 
   group_by(survey.response.id, order.date) |> 
   summarize(
@@ -97,46 +131,44 @@ purchases <- purchases |>
   ) |> 
   ungroup()
 
-
-
-
+# merge the top categories summary & shipping address state into purchases
+purchases <- purchases |>
+  left_join(top_categories_by_id, by = "survey.response.id") |>
+  left_join(state_by_id, by = "survey.response.id")
 
 # Full join datasets on Survey Response ID
 data <- full_join(
   purchases,
   survey,
-  by = join_by(`Survey ResponseID` == Survey.ResponseID)
+  by = join_by(survey.response.id == survey.response.id)
 )
 
 ## rename variables to lower.case.dot.space
-
 data <- data |>
   clean_names(sep_out=".")
 
-## Add numeric variable for numeric factors
+## add numeric variable for numeric factors
+# data <- data |>
+#   mutate(
+#     q.amazon.use.howmany = factor(q.amazon.use.howmany, ordered = TRUE),
+#     q.amazon.use.hh.size = factor(q.amazon.use.hh.size, ordered = TRUE)
+#   ) |>
+#   mutate(
+#     q.amazon.use.howmany.num = as.numeric(q.amazon.use.howmany),
+#     .after = q.amazon.use.howmany
+#   )
 
-data <- data |>
-  mutate(
-    q.amazon.use.howmany = factor(q.amazon.use.howmany, ordered = TRUE),
-    q.amazon.use.hh.size = factor(q.amazon.use.hh.size, ordered = TRUE)
-  ) |>
-  mutate(
-    q.amazon.use.howmany.num = as.numeric(q.amazon.use.howmany),
-    .after = q.amazon.use.howmany
-  )
 
-
-## Dropping Duplicate and Redundant Variables
-# Note: Input names updated to lower case matches
-duplicates <- c(
+## dropping duplicate and redundant variables
+redundant <- c(
   "title",
   "asin.isbn.product.code",
   "q.demos.state",
   "q.amazon.use.hh.size"
 )
-data <- data %>% select(setdiff(colnames(data), duplicates))
+data <- data %>% select(setdiff(colnames(data), redundant))
 
-## Variables Related to Selling Data Questions
+## dropping variables related to selling data questions 
 selling_variables <- c(
   "q.sell.your.data",
   "q.sell.consumer.data",
@@ -145,253 +177,184 @@ selling_variables <- c(
   "q.research.society",
   "test"
 )
+data <- data %>% select(setdiff(colnames(data), selling_variables))
 
-## Create an Encoded Binary Variable Based on if shared accounts
-data <- data %>%
-  mutate(
-    # Renamed from sharedAccount to shared.account
-    shared.account = case_when(
-      # keep NA as NA
-      is.na(q.amazon.use.howmany) ~ NA,
-
-      # anything starting with 1 is FALSE
-      grepl("^1", q.amazon.use.howmany) ~ 0,
-
-      # all other values are TRUE
-      TRUE ~ 1
+## create an encoded variable based on if accounts are shared or not
+data <- data %>% 
+  mutate(shared.account = case_when(
+    # if the value is NA, keep it as NA for now 
+    is.na(q.amazon.use.howmany) ~ NA, 
+    # anything starting with 1 is encoded as 0
+    grepl("^1", q.amazon.use.howmany) ~ 0, 
+    # all other values are encoded as 1 
+    TRUE ~ 1 
     )
   )
 
-## Extract order Year and Month to prepare for encoding
-# make sure it's date format (updated Order.Date -> order.date)
-data$order.date <- as.Date(data$order.date)
-
-# extract year and month
-# Renamed from orderYear/orderMonth to order.year/order.month
-data$order.year <- format(data$order.date, "%Y")
-data$order.month <- format(data$order.date, "%m")
-data$order.year <- factor(data$order.year)
-data$order.month <- factor(data$order.month)
-
-## Drop rows where the category column is an empty string ("")
-# Renamed Category -> category
-data <- subset(data, category != "")
-
-## Keep only rows where category is in the top 500 most frequent categories
-freq <- sort(table(data$category), decreasing = TRUE)
-top_categories <- names(freq)[1:500]
-data <- subset(data, category %in% top_categories)
-
-## Umbrella groups for the category variable
+## hot encoding for the multiple top categories variable 
 data <- data %>%
   mutate(
-    category = case_when(
-      str_detect(
-        category,
-        regex("BOOK|MUSIC|DVD", ignore_case = TRUE)
-      ) ~ "Media",
-      str_detect(
-        category,
-        regex("TOY|GAME|FIGURE|PUZZLE", ignore_case = TRUE)
-      ) ~ "Games",
-      str_detect(
-        category,
-        regex(
-          "SHIRT|PANTS|DRESS|SHORTS|SWIMWEAR|SWEATER|SWEATSHIRT|BRA|UNDERPANTS|PAJAMAS|TUNIC|LEOTARD|ONE_PIECE_OUTFIT|APPAREL",
-          ignore_case = TRUE
-        )
-      ) ~ "Clothing",
-      str_detect(category, regex("PET|ANIMAL", ignore_case = TRUE)) ~ "Pet",
-      str_detect(
-        category,
-        regex(
-          "FOOD|SNACK|COFFEE|TEA|JUICE|MILK|BREAD|CHOCOLATE|SAUCE|SUGAR|NOODLE|RICE|PASTRY",
-          ignore_case = TRUE
-        )
-      ) ~ "Food",
-      str_detect(
-        category,
-        regex(
-          "BEAUTY|SKIN|HAIR|COSMETIC|FRAGRANCE|LIP|MASCARA|NAIL",
-          ignore_case = TRUE
-        )
-      ) ~ "Beauty",
-      str_detect(
-        category,
-        regex(
-          "ELECTRONIC|PHONE|HEADPHONES|CAMERA|MONITOR|FLASH|COMPUTER|TABLET|ACCESSORY|INPUT|DEVICE|DRIVE",
-          ignore_case = TRUE
-        )
-      ) ~ "Electronics",
-      str_detect(
-        category,
-        regex(
-          "HOME|FURNITURE|LAMP|LIVING|DECOR|BED|BATH|KITCHEN|STORAGE|CLEANING|APPLIANCE|TOOL|LIGHT",
-          ignore_case = TRUE
-        )
-      ) ~ "Home",
-      str_detect(
-        category,
-        regex("SPORT|EXERCISE|FITNESS|RECREATION", ignore_case = TRUE)
-      ) ~ "Fitness",
-      str_detect(
-        category,
-        regex(
-          "HEALTH|MEDICATION|SUPPLEMENT|VITAMIN|THERMOMETER|ORTHOPEDIC|PROTECTOR",
-          ignore_case = TRUE
-        )
-      ) ~ "Health",
-      str_detect(
-        category,
-        regex(
-          "STATIONERY|PAPER|MARKING|INK|PEN|WRITING|NOTEBOOK",
-          ignore_case = TRUE
-        )
-      ) ~ "Stationery",
-      TRUE ~ "Other"
-    )
+    category.media = ifelse(
+      str_detect(top.categories, 
+        regex("Media", ignore_case = TRUE)), 
+      1, 0),
+    category.games = ifelse(
+      str_detect(top.categories, 
+        regex("Games", ignore_case = TRUE)), 
+      1, 0),
+    category.clothing = ifelse(
+      str_detect(top.categories, 
+        regex("Clothing", ignore_case = TRUE)), 
+      1, 0),
+    category.pet = ifelse(
+      str_detect(top.categories, 
+        regex("Pet", ignore_case = TRUE)), 
+      1, 0),
+    category.food = ifelse(
+      str_detect(top.categories, 
+        regex("Food", ignore_case = TRUE)), 
+      1, 0),
+    category.beauty = ifelse(
+      str_detect(top.categories, 
+        regex("Beauty", ignore_case = TRUE)), 
+      1, 0),
+    category.electronics = ifelse(
+      str_detect(top.categories, 
+        regex("Electronics", ignore_case = TRUE)), 
+      1, 0),
+    category.home = ifelse(
+      str_detect(top.categories, 
+        regex("Home", ignore_case = TRUE)), 
+      1, 0),
+    category.fitness = ifelse(
+      str_detect(top.categories, 
+        regex("Fitness", ignore_case = TRUE)), 
+      1, 0),
+    category.health = ifelse(
+      str_detect(top.categories, 
+        regex("Health", ignore_case = TRUE)), 
+      1, 0),
+    category.stationery = ifelse(
+      str_detect(top.categories, 
+        regex("Stationery", ignore_case = TRUE)), 
+      1, 0),
+    category.other = ifelse(top.categories == "Other", 1, 0)
   )
 
-## Hot encoding for the multiple choice Life Changes variable
+
+## hot encoding for the multiple choice life changes variable
 data <- data %>%
   mutate(
-    # create multi-hot encoded columns (changed underscore to dot)
     change.employment = ifelse(
-      str_detect(q.life.changes, regex("Lost a job", ignore_case = TRUE)),
-      1,
-      0
-    ),
+      str_detect(q.life.changes, 
+        regex("Lost a job", ignore_case = TRUE)),
+      1, 0),
     change.relocation = ifelse(
-      str_detect(
-        q.life.changes,
-        regex("Moved place of residence", ignore_case = TRUE)
-      ),
-      1,
-      0
-    ),
+      str_detect(q.life.changes, 
+        regex("Moved place of residence", ignore_case = TRUE)),
+      1, 0),
     change.relationship = ifelse(
-      str_detect(q.life.changes, regex("Divorce", ignore_case = TRUE)),
-      1,
-      0
-    ),
+      str_detect(q.life.changes, 
+        regex("Divorce", ignore_case = TRUE)),
+      1, 0),
     change.family = ifelse(
-      str_detect(
-        q.life.changes,
-        regex("Became pregnant|Had a child", ignore_case = TRUE)
-      ),
-      1,
-      0
-    ),
-    # No categories selected
+      str_detect(q.life.changes, 
+        regex("Became pregnant|Had a child", ignore_case = TRUE)),
+      1, 0),
+    # no life changes selected
     change.none = ifelse(q.life.changes == "", 1, 0)
   )
 
-## Race Variables
+## hot endcoding race variables
 data <- data %>%
   mutate(
-    # changed underscore to dot
     race.white = ifelse(
-      str_detect(q.demos.race, regex("White|Caucasian", ignore_case = TRUE)),
-      1,
-      0
-    ),
+      str_detect(q.demos.race, 
+        regex("White|Caucasian", ignore_case = TRUE)),
+      1, 0),
     race.black = ifelse(
       str_detect(
         q.demos.race,
         regex("Black|African American", ignore_case = TRUE)
       ),
-      1,
-      0
-    ),
+      1, 0),
     race.asian = ifelse(
-      str_detect(q.demos.race, regex("Asian", ignore_case = TRUE)),
-      1,
-      0
-    ),
+      str_detect(q.demos.race, 
+        regex("Asian", ignore_case = TRUE)),
+      1, 0),
     race.native.american = ifelse(
       str_detect(
         q.demos.race,
         regex(
           "American Indian|Native American|Alaska Native",
-          ignore_case = TRUE
-        )
-      ),
-      1,
-      0
-    ),
+          ignore_case = TRUE)),
+      1, 0),
     race.pacific.islander = ifelse(
       str_detect(
         q.demos.race,
-        regex("Native Hawaiian|Pacific Islander", ignore_case = TRUE)
-      ),
-      1,
-      0
-    ),
+        regex("Native Hawaiian|Pacific Islander", ignore_case = TRUE)),
+      1, 0),
     race.other = ifelse(
-      str_detect(q.demos.race, regex("Other", ignore_case = TRUE)),
-      1,
-      0
-    )
+      str_detect(
+        q.demos.race, 
+        regex("Other", ignore_case = TRUE)),
+      1, 0)
   )
 
-## Drop the columns we encoded/prepared for encoding
-# Fixed "data$Order.Date" bug (removed 'data$') and lowercased
+## drop the columns we encoded
 data <- data %>%
   select(setdiff(
     colnames(data),
-    c("q.amazon.use.howmany", "q.life.changes", "q.demos.race", "order.date")
+    c("q.amazon.use.howmany", "q.life.changes", "q.demos.race", "top.categories")
   ))
 
-## Drop unnecessary columns
-cols_to_drop <- c(
-  "survey.responseid",
-  "order.date",
-  "shipping.address.state",
-  "title",
-  "asin.isbn.product.code",
-  "q.demos.education",
-  "q.demos.gender",
-  "q.sexual.orientation",
-  "q.demos.state",
-  "q.amazon.use.hh.size",
-  "q.amazon.use.how.oft",
-  "q.substance.use.cigarettes",
-  "q.substance.use.marijuana",
-  "q.substance.use.alcohol",
-  "q.personal.diabetes",
-  "q.personal.wheelchair",
-  "q.sell.your.data",
-  "q.sell.consumer.data",
-  "q.small.biz.use",
-  "q.census.use",
-  "q.research.society",
-  "test"
-)
-cleaned_data <- data %>% select(setdiff(colnames(data), cols_to_drop))
+# ## Drop unnecessary columns
+# cols_to_drop <- c(
+#   "survey.responseid",
+#   "order.date",
+#   "shipping.address.state",
+#   "title",
+#   "asin.isbn.product.code",
+#   "q.demos.education",
+#   "q.demos.gender",
+#   "q.sexual.orientation",
+#   "q.demos.state",
+#   "q.amazon.use.hh.size",
+#   "q.amazon.use.how.oft",
+#   "q.substance.use.cigarettes",
+#   "q.substance.use.marijuana",
+#   "q.substance.use.alcohol",
+#   "q.personal.diabetes",
+#   "q.personal.wheelchair",
+#   "q.sell.your.data",
+#   "q.sell.consumer.data",
+#   "q.small.biz.use",
+#   "q.census.use",
+#   "q.research.society",
+#   "test"
+# )
+# cleaned_data <- data %>% select(setdiff(colnames(data), cols_to_drop))
 
 
-## Convert all character and logical variables to factors
+## convert all character and logical variables to factors
 data <- data %>%
   mutate(
     across(where(is.character), as.factor),
     across(where(is.logical), as.factor)
   )
 
-
-## Handle Missing Data
+## handle missing data
 imputation_pipeline <- gunion(list(
   # numeric features: regression tree imputation
   po("select", id = "sel_num", selector = selector_type("numeric")) %>>%
     po("imputelearner", id = "imp_num", learner = lrn("regr.rpart")),
-
-  # factor / ordered features:classification tree imputation
+  # factor / ordered features: classification tree imputation
   po(
     "select",
     id = "sel_cat",
     selector = selector_type(c("factor", "ordered"))
   ) %>>%
     po("imputelearner", id = "imp_cat", learner = lrn("classif.rpart")),
-
   # everything else (IDs, dates, etc.) just passed through
   po(
     "select",
@@ -401,22 +364,22 @@ imputation_pipeline <- gunion(list(
 )) %>>%
   po("featureunion")
 
-## Encode Variables
-# impute variables
-full_pipeline <- imputation_pipeline %>>%
-  po("encode", method = "one-hot")
-# defining the task
-# Updated target variable to lower case
-housing_tsk <- as_task_regr(data, target = "q.amazon.use.hh.size.num")
-# train pipeline on the data
-full_pipeline$train(housing_tsk)
-# transform the data
-encoded_data <- full_pipeline$predict(housing_tsk)$task$data()
+# ## encode all other categorical variables
+# full_pipeline <- imputation_pipeline %>>%
+#   po("encode", method = "one-hot")
+# # data without the id variable 
+# no_id_data <- data %>% select(setdiff(colnames(data), "survey.response.id"))
+# # defining the task
+# housing_tsk <- as_task_regr(no_id_data, target = "q.amazon.use.hh.size.num")
+# # train pipeline on the data
+# full_pipeline$train(housing_tsk)
+# # impute the data
+# inputed_data <- full_pipeline$predict(housing_tsk)$task$data()
 
 # Create learner
 lrn_xgb <- as_learner(
   imputation_pipeline %>>%
-    ## one-hot encode our categorical variables
+    ## one-hot encode our variables
     po("encode", method = "one-hot") %>>%
 
     ## pass cleaned dataset into an XGBoost regression model
