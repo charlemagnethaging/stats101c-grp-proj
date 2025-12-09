d8f8fe91eec173fc9f4c42dba0a16788a637d1cb
charlemagnethaging <ckopp313@gmail.com>
2025-12-08 16:10:37 -0800

    updated visuals (not finished, only up til category visualizations)

--- regression/regression_training.R
+++ regression/regression_training.R
@@ -13,29 +13,29 @@ library(glmnet)
 library(stringr)
 library(janitor)
 library(mlr3viz)
-library(ranger)  # backend for regr.ranger (usually pulled in by mlr3learners, but safe)
+library(ranger) # backend for regr.ranger (usually pulled in by mlr3learners, but safe)
 
-load("processed_data_train.RData")  # this should create train_data
+load("processed_data_train.RData") # this should create train_data
 ## handle missing data
 imputation_pipeline <- gunion(list(
-  # numeric features: regression tree imputation
-  po("select", id = "sel_num", selector = selector_type("numeric")) %>>%
-    po("imputelearner", id = "imp_num", learner = lrn("regr.rpart")),
-  # factor / ordered features: classification tree imputation
-  po(
-    "select",
-    id = "sel_cat",
-    selector = selector_type(c("factor", "ordered"))
-  ) %>>%
-    po("imputelearner", id = "imp_cat", learner = lrn("classif.rpart")),
-  # everything else (IDs, dates, etc.) just passed through
-  po(
-    "select",
-    id = "sel_rest",
-    selector = selector_invert(selector_type(c("numeric", "factor", "ordered")))
-  )
+	# numeric features: regression tree imputation
+	po("select", id = "sel_num", selector = selector_type("numeric")) %>>%
+		po("imputelearner", id = "imp_num", learner = lrn("regr.rpart")),
+	# factor / ordered features: classification tree imputation
+	po(
+		"select",
+		id = "sel_cat",
+		selector = selector_type(c("factor", "ordered"))
+	) %>>%
+		po("imputelearner", id = "imp_cat", learner = lrn("classif.rpart")),
+	# everything else (IDs, dates, etc.) just passed through
+	po(
+		"select",
+		id = "sel_rest",
+		selector = selector_invert(selector_type(c("numeric", "factor", "ordered")))
+	)
 )) %>>%
-  po("featureunion")
+	po("featureunion")
 
 # defining the task
 housing_tsk <- as_task_regr(train_data, target = "q.amazon.use.hh.size.num")
@@ -44,7 +44,7 @@ housing_tsk <- as_task_regr(train_data, target = "q.amazon.use.hh.size.num")
 set.seed(101)
 split <- partition(housing_tsk, ratio = 0.8)
 train_idx <- split$train
-test_idx  <- split$test
+test_idx <- split$test
 
 # measures
 measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
@@ -54,11 +54,11 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 # # head(mod_mat, n = 2)
 # lasso_housing_tsk <- as_task_regr(mod_mat[, -1], target = "q.amazon.use.hh.size.num")
 
-# lrn_lasso_cv <- lrn("regr.cv_glmnet", 
-#   alpha = 1, 
-#   nfolds = 10, 
-#   s = "lambda.min", 
-#   lambda = 10^seq(from = -1.5, to = 1.5, by = 0.1), 
+# lrn_lasso_cv <- lrn("regr.cv_glmnet",
+#   alpha = 1,
+#   nfolds = 10,
+#   s = "lambda.min",
+#   lambda = 10^seq(from = -1.5, to = 1.5, by = 0.1),
 #   standardize = TRUE
 # )
 
@@ -88,7 +88,6 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 # preds
 # preds$score(measures)
 
-
 # ## -----------------------------------------------------------
 # # xgbost learner 2
 # lrn_xgboost2 <- as_learner(
@@ -105,14 +104,14 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 #     )
 # )
 
-# # 5-fold cross-validation resampling method 
+# # 5-fold cross-validation resampling method
 # resampling <- rsmp("cv", folds = 5)
 
 # # terminating condition
 # terminator <- trm("evals", n_evals = 20)
 
 # # grid search tuner
-# tuner_grid <- tnr("grid_search", resolution = 3) 
+# tuner_grid <- tnr("grid_search", resolution = 3)
 
 # # autoturner
 # at_xgboost <- auto_tuner(
@@ -149,7 +148,7 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 
 # # tuning instance
 # set.seed(101)
-# instance <- ti(task = housing_tsk, 
+# instance <- ti(task = housing_tsk,
 #   learner = lrn_xgboost3,
 #   resampling = rsmp("cv", folds = 5),
 #   measures = list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse")),
@@ -157,7 +156,7 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 # )
 
 # # grid search
-# tuner <- tnr("grid_search", resolution = 10) 
+# tuner <- tnr("grid_search", resolution = 10)
 # tuner$optimize(inst = instance)
 
 # # define learner
@@ -165,7 +164,7 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 # # optimal parameters
 # instance$result
 # optimal_params <- instance$result_learner_param_vals
-# # set optimal hyperparamters 
+# # set optimal hyperparamters
 # lrn_rpart_tuned$param_set$values <- optimal_params
 # # train the fit parameters
 # lrn_rpart_tuned$train(housing_tsk, row_ids = train_idx)
@@ -173,7 +172,6 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 # preds3 <- lrn_rpart_tuned$predict(housing_tsk, row_ids = test_idx)
 # preds3
 # preds3$score(measures)
-
 
 # ## random forest learner -----------------------------------------------------
 # lrn_rf1 <- as_learner(
@@ -190,10 +188,10 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 #     )
 # )
 
-# ## train RF 
+# ## train RF
 # lrn_rf1$train(housing_tsk, row_ids = train_idx)
 
-# ## test RF 
+# ## test RF
 # preds4 <- lrn_rf$predict(housing_tsk, row_ids = test_idx)
 # preds4$score(measures)
 
@@ -204,21 +202,17 @@ measures <- list(msr("regr.rmse"), msr("regr.mae"), msr("regr.mse"))
 ## For Trevor
 ## rf learner 2 ---------------------------------------------------
 lrn_rf2 <- as_learner(
-  imputation_pipeline %>>%
-    po("encode",  method = "one-hot") %>>%
-    lrn("regr.ranger",
-        num.trees = to_tune(50, 200),
-        min.node.size = to_tune(1, 10),
-        mtry = to_tune(1, 8)
-    )
+	imputation_pipeline %>>%
+		po("encode", method = "one-hot") %>>%
+		lrn("regr.ranger", num.trees = to_tune(50, 200), min.node.size = to_tune(1, 10), mtry = to_tune(1, 8))
 )
 
 at_rf2 <- auto_tuner(
-  learner = lrn_rf2,
-  resampling = rsmp("cv", folds = 5),       
-  measure = msr("regr.rmse"),               
-  tuner = tnr("grid_search"),            
-  terminator = trm("evals", n_evals = 20)   
+	learner = lrn_rf2,
+	resampling = rsmp("cv", folds = 5),
+	measure = msr("regr.rmse"),
+	tuner = tnr("grid_search"),
+	terminator = trm("evals", n_evals = 20)
 )
 
 # train model
